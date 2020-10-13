30339d186d9e77a011828365dc696a3bf6d4c9c8
clust3rr <thg.santos@outlook.com>
2020-10-13 14:38:26 -0300

    created the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import Routes from './routes'
+
 import './style/global.css'
-import './style/pages/landing.css'
 
 function App() {
   return (
-    <div id="page-landing">
-      <h1>Hello World</h1>
-    </div>
+    <Routes />
   );
 }
 
---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OrphanagesMap() {
+    return (
+        <div>Hello World</div>
+    )
+}
+
+export default OrphanagesMap
