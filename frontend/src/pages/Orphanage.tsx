25220b6d655f69c2d3a08595db1f90bbd5199e9a
clust3rr <thg.santos@outlook.com>
2020-10-14 23:34:50 -0300

    created the form

--- frontend/src/pages/Orphanage.tsx
+++ frontend/src/pages/Orphanage.tsx
@@ -86,7 +86,7 @@ export default function Orphanage() {
               </Map>
 
               <footer>
-                <a target="_blank" href={`https://www.google.com/maps/dir/?api=1&destination=${orphanage.latitude},${orphanage.longitude}`}>Ver rotas no Google Maps</a>
+                <a target="_blank" rel="noopener noreferrer" href={`https://www.google.com/maps/dir/?api=1&destination=${orphanage.latitude},${orphanage.longitude}`}>Ver rotas no Google Maps</a>
               </footer>
             </div>
 
